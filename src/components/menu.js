2af8806aff4e4919c052553d3bc43bdf8a87829d
Avilin <avilin.persson@gmail.com>
2021-04-13 14:17:37 +0200

    second commit

---
+++ src/components/menu.js
@@ -0,0 +1,22 @@
+
+import React from "react"; // obligatorisk i varje react-fil, kan få error 
+
+// Menu måste ha stor bokstav, kommer inte funka annars
+function Menu() { 
+
+    // funktionen måste returnera JSX 
+    return (
+
+        <nav>
+
+            jsx // htlm element 
+
+        </nav>
+    )
+}
+
+// måste exportera funktionen så den kan användas/importera i tex App.js
+export default Menu; 
+
+
+
